d8d95b6a3cc1db8b7b14be1e641022f60a6f4474
lanou3g <zhaoce@yangyingda.local>
2016-06-07 19:46:06 +0800

    1

--- PZSGitTest/PZSGitTest/ViewController.m
+++ PZSGitTest/PZSGitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"第一次提交");
+    NSLog(@"2");
 }
 
 - (void)didReceiveMemoryWarning {
